36f51ea935e70a4e58eaf7ae65e4141bb818a474
hrpatel <pate3900+github@gmail.com>
2016-09-18 22:19:38 -0400

    comments, output message, sanitize input for empty

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,14 +6,25 @@
 
   MsgController.$inject = ['$scope'];
   function MsgController($scope) {
+    // initialize part of the model
     $scope.lunchItems = "";
 
+    // initial handler for the check button
     $scope.checkLunch = function () {
-
+      // validate, count input
       var num_items = countItems();
 
+      // Output according to the validated/sanitized input
       if (num_items == 0) {
         $scope.lunchMessage = "Please enter data first";
+      }
+      else if (num_items <= 3) {
+        $scope.lunchMessage = "Enjoy!";
+      }
+      else if (num_items > 3) {
+        $scope.lunchMessage = "Too much!";
+      } else {
+        console.log("This should never happen!")
       }
     };
 
@@ -21,7 +32,19 @@
     function countItems () {
       var num_items = 0;
 
-      return 0;
+      if ($scope.lunchItems.length > 0) {
+        var items = $scope.lunchItems.split(",");
+
+        // remove empty items from the list
+        // do NOT consider and empty item, i.e.", , ," as an item towards to the count
+        items = items.filter(function (item) {
+          return item != "";
+        });
+
+        num_items = items.length;
+      }
+
+      return num_items;
     }
   }
 })();
