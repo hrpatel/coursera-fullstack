fff3bc7d5745c8f2ced91f6d60f29f758dcdfd9f
hrpatel <pate3900+github@gmail.com>
2016-09-18 21:46:09 -0400

    style, setup module/controller/model, utility method

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,12 +1,27 @@
 (function () {
-'use strict';
+  'use strict';
 
-angular.module('MsgApp', [])
-.controller('MsgController', MsgController);
+  angular.module('LunchCheck', [])
+    .controller('LunchCheckController', MsgController);
 
-MsgController.$inject = ['$scope'];
-function MsgController($scope) {
-  $scope.name = "Yaakov";
-}
+  MsgController.$inject = ['$scope'];
+  function MsgController($scope) {
+    $scope.lunchItems = "";
 
+    $scope.checkLunch = function () {
+
+      var num_items = countItems();
+
+      if (num_items == 0) {
+        $scope.lunchMessage = "Please enter data first";
+      }
+    };
+
+    // Utility function to sanitize input and count items
+    function countItems () {
+      var num_items = 0;
+
+      return 0;
+    }
+  }
 })();
