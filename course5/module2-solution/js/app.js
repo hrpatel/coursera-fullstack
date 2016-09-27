dfc586bc5d6a5275a648a8e2a7fdc2bcfc844cdb
hrpatel <pate3900+github@gmail.com>
2016-09-26 23:44:02 -0400

    comments, fix typo, spacing

--- course5/module2-solution/js/app.js
+++ course5/module2-solution/js/app.js
@@ -1,35 +1,41 @@
 (function () {
+
   'use strict';
 
+  // setup module, service and controllers
   angular.module('ShoppingListCheckOff', [])
     .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
     .controller('ToBuyController', ToBuyController)
     .controller('AlreadyBoughtController', AlreadyBoughtController);
 
+  // controller to handle list of items to buy
   ToBuyController.$inject = ['ShoppingListCheckOffService'];
   function ToBuyController(ShoppingListCheckOffService) {
     var toBuy = this;
 
+    // initialize shopping list
     toBuy.items = ShoppingListCheckOffService.getItemsToBuy();
 
+    // mark items at itenIndex bought
     toBuy.buyItem = function (itemIndex) {
       ShoppingListCheckOffService.buyItem(itemIndex);
     };
   }
 
-
+  // controller to handle list of bought items
   AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
   function AlreadyBoughtController(ShoppingListCheckOffService) {
     var bought = this;
 
+    // initialize list of bought items
     bought.items = ShoppingListCheckOffService.getBoughtItems();
   }
 
-
+  // service to handle shopping list of items to buy and items already bought
   function ShoppingListCheckOffService() {
     var service = this;
 
-    // List of shopping items
+    // sample list of items to buy
     var itemsToBuy = [
       { name: "cookies", quantity: 10 },
       { name: "chips", quantity: 7 },
@@ -38,18 +44,22 @@
       { name: "apples", quantity: 5 }
     ];
 
+    // initialize array of items we've 'bought'
     var itemsBought = [];
 
-    service.buyItem = function (itemIdex) {
+    // move item from toBuy to 'bought' list
+    service.buyItem = function (itemIndex) {
       itemsBought.push(
-        itemsToBuy.splice(itemIdex, 1)[0]
+        itemsToBuy.splice(itemIndex, 1)[0]
       );
     };
 
+    // get list of items to buy
     service.getItemsToBuy = function () {
       return itemsToBuy;
     };
 
+    // get list of items already bought
     service.getBoughtItems = function () {
       return itemsBought;
     };
