bd41dba540ccbd0fc9d42455477ebdff2c2a7a32
hrpatel <pate3900+github@gmail.com>
2016-10-04 19:08:08 -0400

    update code style

--- course5/module3-solution/js/app.js
+++ course5/module3-solution/js/app.js
@@ -1,89 +1,89 @@
 (function () {
-'use strict';
+  'use strict';
 
-angular.module('NarrowItDownApp', [])
-  .controller('NarrowItDownController', NarrowItDownController)
-  .service('MenuSearchService', MenuSearchService)
-  .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
-  .component('shoppingList', {
-    templateUrl: 'shoppingList.html',
-    controller: ShoppingListComponentController,
-    bindings: {
-      items: '<',
-      myTitle: '@title',
-      onRemove: '&'
-    }
-  });
-
-  ShoppingListComponentController.$inject = ['$scope', '$element']
-function ShoppingListComponentController($scope, $element) {
-  var $ctrl = this;
-
-  $ctrl.cookiesInList = function () {
-    for (var i = 0; i < $ctrl.items.length; i++) {
-      var name = $ctrl.items[i].name;
-      if (name.toLowerCase().indexOf("cookie") !== -1) {
-        return true;
-      }
-    }
-
-    return false;
-  };
-
-  $ctrl.remove = function (myIndex) {
-    $ctrl.onRemove({ index: myIndex });
-  };
-
-  $ctrl.$onInit = function () {
-    console.log("We are in $onInit()");
-  };
-
-  $ctrl.$onChanges = function (changeObj) {
-    console.log("Changes: ", changeObj);
-  }
-
-  $ctrl.$postLink = function () {
-    $scope.$watch('$ctrl.cookiesInList()', function (newValue, oldValue) {
-      console.log($element);
-      if (newValue === true) {
-        // Show warning
-        var warningElem = $element.find('div.error');
-        warningElem.slideDown(900);
-      }
-      else {
-        // Hide warning
-        var warningElem = $element.find('div.error');
-        warningElem.slideUp(900);
+  angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
+    .component('shoppingList', {
+      templateUrl: 'shoppingList.html',
+      controller: ShoppingListComponentController,
+      bindings: {
+        items: '<',
+        myTitle: '@title',
+        onRemove: '&'
       }
     });
-  };
-}
+
+  ShoppingListComponentController.$inject = ['$scope', '$element']
+  function ShoppingListComponentController($scope, $element) {
+    var $ctrl = this;
+
+    $ctrl.cookiesInList = function () {
+      for (var i = 0; i < $ctrl.items.length; i++) {
+        var name = $ctrl.items[i].name;
+        if (name.toLowerCase().indexOf("cookie") !== -1) {
+          return true;
+        }
+      }
+
+      return false;
+    };
+
+    $ctrl.remove = function (myIndex) {
+      $ctrl.onRemove({ index: myIndex });
+    };
+
+    $ctrl.$onInit = function () {
+      console.log("We are in $onInit()");
+    };
+
+    $ctrl.$onChanges = function (changeObj) {
+      console.log("Changes: ", changeObj);
+    }
+
+    $ctrl.$postLink = function () {
+      $scope.$watch('$ctrl.cookiesInList()', function (newValue, oldValue) {
+        console.log($element);
+        if (newValue === true) {
+          // Show warning
+          var warningElem = $element.find('div.error');
+          warningElem.slideDown(900);
+        }
+        else {
+          // Hide warning
+          var warningElem = $element.find('div.error');
+          warningElem.slideUp(900);
+        }
+      });
+    };
+  }
 
 
   NarrowItDownController.$inject = ['MenuSearchService'];
-function NarrowItDownController(MenuSearchService) {
-  var menu = this;
+  function NarrowItDownController(MenuSearchService) {
+    var menu = this;
 
-  var promise = MenuSearchService.getMenuCategories();
-
-  promise.then(function (response) {
-    menu.categories = response.data;
-  })
-    .catch(function (error) {
-      console.log("Something went terribly wrong.");
-    });
-
-  menu.logMenuItems = function (shortName) {
-    var promise = MenuSearchService.getMenuForCategory(shortName);
+    var promise = MenuSearchService.getMenuCategories();
 
     promise.then(function (response) {
-      console.log(response.data);
+      menu.categories = response.data;
     })
       .catch(function (error) {
-        console.log(error);
+        console.log("Something went terribly wrong.");
+      });
+
+    menu.logMenuItems = function (shortName) {
+      var promise = MenuSearchService.getMenuForCategory(shortName);
+
+      promise.then(function (response) {
+        console.log(response.data);
       })
-  };
-}
+        .catch(function (error) {
+          console.log(error);
+        })
+    };
+  }
 
 
   MenuSearchService.$inject = ['$http', 'ApiBasePath']
