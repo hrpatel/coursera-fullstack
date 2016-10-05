4a9769b84cae9052172e897cb0b6d445ed4b9f32
hrpatel <pate3900+github@gmail.com>
2016-10-05 15:41:44 -0400

    setup http service and filter results from returned data

--- course5/module3-solution/js/app.js
+++ course5/module3-solution/js/app.js
@@ -5,113 +5,108 @@
     .controller('NarrowItDownController', NarrowItDownController)
     .service('MenuSearchService', MenuSearchService)
     .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
-    .component('shoppingList', {
-      templateUrl: 'shoppingList.html',
-      controller: ShoppingListComponentController,
-      bindings: {
-        items: '<',
-        myTitle: '@title',
-        onRemove: '&'
-      }
-    });
+    // .component('shoppingList', {
+    //   templateUrl: 'shoppingList.html',
+    //   controller: ShoppingListComponentController,
+    //   bindings: {
+    //     items: '<',
+    //     myTitle: '@title',
+    //     onRemove: '&'
+    //   }
+    // });
+  ;
 
-  ShoppingListComponentController.$inject = ['$scope', '$element']
-  function ShoppingListComponentController($scope, $element) {
-    var $ctrl = this;
-
-    $ctrl.cookiesInList = function () {
-      for (var i = 0; i < $ctrl.items.length; i++) {
-        var name = $ctrl.items[i].name;
-        if (name.toLowerCase().indexOf("cookie") !== -1) {
-          return true;
-        }
-      }
-
-      return false;
-    };
-
-    $ctrl.remove = function (myIndex) {
-      $ctrl.onRemove({ index: myIndex });
-    };
-
-    $ctrl.$onInit = function () {
-      console.log("We are in $onInit()");
-    };
-
-    $ctrl.$onChanges = function (changeObj) {
-      console.log("Changes: ", changeObj);
-    }
-
-    $ctrl.$postLink = function () {
-      $scope.$watch('$ctrl.cookiesInList()', function (newValue, oldValue) {
-        console.log($element);
-        if (newValue === true) {
-          // Show warning
-          var warningElem = $element.find('div.error');
-          warningElem.slideDown(900);
-        }
-        else {
-          // Hide warning
-          var warningElem = $element.find('div.error');
-          warningElem.slideUp(900);
-        }
-      });
-    };
-  }
+  // ShoppingListComponentController.$inject = ['$scope', '$element']
+  // function ShoppingListComponentController($scope, $element) {
+  //   var $ctrl = this;
+  //
+  //   $ctrl.cookiesInList = function () {
+  //     for (var i = 0; i < $ctrl.items.length; i++) {
+  //       var name = $ctrl.items[i].name;
+  //       if (name.toLowerCase().indexOf("cookie") !== -1) {
+  //         return true;
+  //       }
+  //     }
+  //
+  //     return false;
+  //   };
+  //
+  //   $ctrl.remove = function (myIndex) {
+  //     $ctrl.onRemove({ index: myIndex });
+  //   };
+  //
+  //   $ctrl.$onInit = function () {
+  //     console.log("We are in $onInit()");
+  //   };
+  //
+  //   $ctrl.$onChanges = function (changeObj) {
+  //     console.log("Changes: ", changeObj);
+  //   }
+  //
+  //   $ctrl.$postLink = function () {
+  //     $scope.$watch('$ctrl.cookiesInList()', function (newValue, oldValue) {
+  //       console.log($element);
+  //       if (newValue === true) {
+  //         // Show warning
+  //         var warningElem = $element.find('div.error');
+  //         warningElem.slideDown(900);
+  //       }
+  //       else {
+  //         // Hide warning
+  //         var warningElem = $element.find('div.error');
+  //         warningElem.slideUp(900);
+  //       }
+  //     });
+  //   };
+  // }
 
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService) {
-    var menu = this;
+    var narrow = this;
 
-    var promise = MenuSearchService.getMenuCategories();
+    // define defaults
+    narrow.searchTerm = "broth";
+    narrow.foundItems = [];
 
-    promise.then(function (response) {
-      menu.categories = response.data;
-    })
-      .catch(function (error) {
-        console.log("Something went terribly wrong.");
-      });
-
-    menu.logMenuItems = function (shortName) {
-      var promise = MenuSearchService.getMenuForCategory(shortName);
+    narrow.narrowDown = function () {
+      var promise = MenuSearchService.getMatchedMenuItems(narrow.searchTerm);
 
       promise.then(function (response) {
-        console.log(response.data);
-      })
-        .catch(function (error) {
-          console.log(error);
-        })
-    };
+        narrow.foundItems = response;
+      });
+
+      promise.catch(function (error) {
+        console.log("Something went terribly wrong.", error);
+      });
+    }
   }
 
 
-  MenuSearchService.$inject = ['$http', 'ApiBasePath']
+  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
   function MenuSearchService($http, ApiBasePath) {
     var service = this;
 
-    service.getMenuCategories = function () {
-      var response = $http({
+    service.getMatchedMenuItems = function(searchTerm) {
+
+      return $http({
         method: "GET",
-        url: (ApiBasePath + "/categories.json")
-      });
+        url: (ApiBasePath + "/menu_items.json")
+      })
+        .then(
+          function (response) {
+            var foundItems = [];
+            var allItems = response.data['menu_items'];
 
-      return response;
+            for (var i = 0; i < allItems.length; i++) {
+              if (allItems[i]['description'].includes(searchTerm)) {
+                foundItems.push(allItems[i])
+              }
+            }
+
+            return foundItems;
+          });
     };
-
-
-    service.getMenuForCategory = function (shortName) {
-      var response = $http({
-        method: "GET",
-        url: (ApiBasePath + "/menu_items.json"),
-        params: {
-          category: shortName
-        }
-      });
-
-      return response;
-    };
-
   }
 
 })();
